34fb9919fa25450d9c7c7681548a22e5aa9f12a6
ThistleFIN <roosa.virta@gmail.com>
2023-05-07 20:22:33 +0300

    Squashed a bug and the block is now done

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('jquery', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '11fe73df378213284482');
+<?php return array('dependencies' => array('jquery', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '57055e6a1cf2a3703376');
